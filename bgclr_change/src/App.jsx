c0a59fcc819a3e8e55e7d4f60d0ef8db4115799b
ayusshh66 <adonisjefrey66@gmail.com>
2026-02-21 03:38:59 +0530

    1st project

--- bgclr_change/src/App.jsx
+++ bgclr_change/src/App.jsx
@@ -4,21 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [Color, setColor] = useState("white")
+ 
 
   return (
-    <>
-      <div className="screen w-fu
-      ll h-screen">
-        <div className="colorBar flex flex-wrap justify-center ">
-          <div className="colours fixed flex flex-wrap justify-center bottom-12 bg-yellow-950">
-            <button className='bg-red-900'>
-              red
-            </button>
-          </div>
+    
+      <div className='page w-full min-h-full ' style={{ backgroundColor: Color,}}>
+        <div className="box bg-slate-500 rounded-full h-12 flex  justify-around items-center mt-10">
+          <button className='bg-red-500 text-white rounded-full p-2 cursor-pointer' onClick={() => setColor("red")}>red</button>
+          <button className='bg-yellow-500 text-white rounded-full p-2 cursor-pointer' onClick={() => setColor("yellow")}>yellow</button>
+          <button className='bg-blue-500 text-white rounded-full p-2 cursor-pointer' onClick={() => setColor("blue")}>blue</button>
+          <button className='bg-green-500 text-white rounded-full p-2 cursor-pointer' onClick={() => setColor("green")}>green</button>
         </div>
       </div>
-    </>
+    
+    
   )
 }
 
