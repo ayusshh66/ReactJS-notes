2afbf1d1293d03412e324f5936b3842bf98c8f62
ayusshh66 <adonisjefrey66@gmail.com>
2026-03-04 23:34:44 +0530

    contextApi Intro

---
+++ ContextApi/src/context/userContextProvider.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import UserContext from './userContext'
+
+const userContextProvider = ({children}) => {
+        return(
+            <>
+            {children}
+            </>
+
+        )
+
+}
+
